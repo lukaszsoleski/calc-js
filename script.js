a522bc5daf06c8915abafae85c34b947041eb114
Łukasz Soleski <luk.soleski@gmail.com>
2018-04-28 10:25:28 +0100

    keybord support

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 window.onload = function () {addEvents();}
 function addEvents(){
 let chars = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','.'];
-for (let i = 0; i < chars.length; i ++)
-  document.getElementById(chars[i]).addEventListener('click',function(){insert(chars[i]);});
+for (let i = 0; i < chars.length; i ++) document.getElementById(chars[i]).addEventListener('click',function(){insert(chars[i]);});
 
 document.getElementById('=').addEventListener('click',function(){calculate();});
 document.getElementById('<').addEventListener('click',function(){undo();});        
-document.getElementById('C').addEventListener('click',function(){clean();});        
-}
+document.getElementById('C').addEventListener('click',function(){clean();});   
 
+document.addEventListener('keyup',function(e){
+    e.preventDefault();
+    
+    
+    if ((e.keyCode >= 48 && e.keyCode <=57) || (e.keyCode >= 96 && e.keyCode <= 105))
+       {
+        let t =String.fromCharCode(e.keyCode);
+         document.getElementById(t).click();     
+       }
+
+    });
+}
 
 function insert(num){
    let expression = document.getElementById("output-form").getAttribute("value");
    expression += num;
    document.getElementById("output-form").setAttribute("value",expression);
+   console.log("insert");
 }
 function calculate(){
     let expression = document.getElementById("output-form").getAttribute("value");
@@ -28,6 +39,7 @@ function calculate(){
            }//if
        }//catch
     }//if
+    console.log("calculate");
 }//calculate
 function clean(){
     document.getElementById("output-form").setAttribute("value","");
